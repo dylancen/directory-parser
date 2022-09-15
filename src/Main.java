ad52643baf2d0ff63dffb147a4ed91b9ab0741fe
dylancen <dylancenotto@gmail.com>
2022-09-15 17:18:44 +0300

    clean

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
 import java.io.File;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args){
