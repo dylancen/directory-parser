71350045c7ed550ff536ea2cfb7e4c61869bcadf
dylancen <dylancenotto@gmail.com>
2022-09-15 14:59:02 +0300

    Changed ordering to happen during directory parsing, and added error for infinite dependency loop

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 import java.io.File;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args){
         directoryParser parser = new directoryParser("C:\\Users\\dylan\\Documents\\test_folder");
-        for(File file : parser.getAllFiles()){
-            System.out.println(file.getAbsoluteFile());
+        for(File f : parser.getOrderedFiles()){
+            System.out.println(f.getAbsolutePath());
         }
     }
 }
--- src/directoryParser.java
+++ src/directoryParser.java
@@ -1,28 +1,59 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.PrintWriter;
-import java.util.Comparator;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class directoryParser {
-    private Set<File> allFiles;
+    private List<File> order;
     private String originPath;
 
-    private void parseAllFiles(String path){
+    private static final Pattern p = Pattern.compile("(require ')(.*)(')");
+
+    private void parseAndOrderAllFiles(String path){
         File originDir = new File(path);
         for(File file : originDir.listFiles()){
             if(file.isFile()){
-                allFiles.add(file);
+                List<String> dependencies = new ArrayList<String>();
+                addToOrder(file, dependencies);
             }else{
-                parseAllFiles(file.getAbsolutePath());
+                parseAndOrderAllFiles(file.getAbsolutePath());
             }
+        }
+    }
+
+    private void addToOrder(File f, List<String> dependencies){
+        try {
+            Scanner s = new Scanner(f);
+            dependencies.add(f.getAbsolutePath());
+            while(s.hasNextLine()){
+                String line = s.nextLine();
+                String required = reqRegex(line);
+                if(required != null){
+                    File depFile = new File(this.originPath + "\\" + required + ".txt");
+                    if(dependencies.contains(depFile.getAbsolutePath())){
+                        throw new Exception("Infinite loop: " + dependencies.toString());
+                    }else{
+                        addToOrder(depFile, dependencies);
+                    }
+                }
+            }
+            dependencies.remove(f.getAbsolutePath());
+            s.close();
+            if(!order.contains(f)){
+                order.add(f);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
     private void concatFileContents(){
         File newFile = new File("output_file.txt");
+        List<File> ordered = new ArrayList<>();
+        ordered.addAll(this.order);
         try{
             if(!newFile.createNewFile()){
                 PrintWriter clearer = new PrintWriter(newFile);
@@ -32,31 +63,30 @@ public class directoryParser {
 
             FileWriter writer = new FileWriter(newFile);
             Scanner reader;
-            for(File file : allFiles){
-                reader = new Scanner(file);
-                while(reader.hasNextLine()){
-                    writer.write(reader.nextLine());
-                    writer.write("\n");
-                }
-                reader.close();
-            }
-            writer.close();
+
         }catch (Exception e){
             System.out.println("Error occured");
             e.printStackTrace();
         }
     }
 
-    public directoryParser(String path){
-        this.originPath = path;
-        this.allFiles = new TreeSet<>(Comparator.comparing(File::getName));
-        parseAllFiles(this.originPath);
-        concatFileContents();
+    private String reqRegex(String line){
+        Matcher m = p.matcher(line);
+        if(m.find()){
+            return m.group(2);
+        }
+        return null;
     }
 
+    public directoryParser(String path){
+        this.originPath = path;
+        this.order = new ArrayList<>();
 
+        parseAndOrderAllFiles(this.originPath);
+        //concatFileContents();
+    }
 
-    public Set<File> getAllFiles(){
-        return this.allFiles;
+    public List<File> getOrderedFiles(){
+        return this.order;
     }
 }
